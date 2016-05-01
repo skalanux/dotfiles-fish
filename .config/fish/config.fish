dbd06794fd778f72bd9045ab3f99f1eb5b33dd94
Juan Manuel Schillaci <jmschillaci@gmail.com>
2016-05-01 15:08:41 -0300

    Modified path of fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,8 @@
 # Path to Oh My Fish install.
-set -gx OMF_PATH "/home/juanms/.local/share/omf"
+set -gx OMF_PATH "/home/ska/.local/share/omf"
 
 # Customize Oh My Fish configuration path.
-#set -gx OMF_CONFIG "/home/juanms/.config/omf"
+#set -gx OMF_CONFIG "/home/ska/.config/omf"
 
 # Load oh-my-fish configuration.
 source $OMF_PATH/init.fish
